8e6dbd15bede375bdee8fa8899ec553f9c1d2126
Ahmad Soliman <76499801+ahmadSoliman94@users.noreply.github.com>
2023-01-11 00:59:03 +0000

    first step in fastAPI

---
+++ FastAPIGuide/first steps/main.py
@@ -0,0 +1,16 @@
+# import libraries 
+from fastapi import FastAPI
+
+# create a FastAPI instance
+app = FastAPI() 
+
+# define a path operation decorator
+@app.get("/")
+
+# define the path operation function
+async def root():
+
+    #  return the content
+    return {"message": "Hello World"}
+
+# Command: (uvicorn main:app --reload) to run the server. 
